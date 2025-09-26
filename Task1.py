ebb146de204b2db82d9c6d6783a301139d77c810
Bulgur678 <abcd505@yandex.ru>
2025-09-26 15:57:21 +0300

    Stage 2: old version

--- Task1.py
+++ Task1.py
@@ -1,14 +1,17 @@
 import os
 
+
 def act(a):
     b = a.split()
     if a == "exit":
         exit()
     if len(b) == 0:
         return ""
-    if  b[0].startswith("$"):
-        if (os.environ.get(b[0][1:]) != None):
-             return os.environ.get(b[0][1:])
+    if  len(b) > 1:
+        if b[0] == "echo":
+            return a.replace("\n","")[5:]
+        if (os.environ.get(b[1]) != None):
+            b[1] = os.environ.get(b[1])
     if b[0] == "ls":
         return b
     elif b[0] == "cd":
@@ -17,8 +20,17 @@ def act(a):
         return f"{b[0]}: command not found"
 
 if __name__ == "__main__":
+    # Стартовый скрипт для проверки команд
+    with open('start_script.txt') as f:
+        for line in f:
+            res = act(line)
+            print(f"{os.getcwd()} vfs@ {line}{res}")
+            if ("command not found" in res):
+                break
+
     while True:
-        a = input("vfs@ ")
+        a = input(f"{os.getcwd()} vfs@: ")
         result = act(a)
         if result:  # Чтобы не выводить пустые строки
             print(result)
+
