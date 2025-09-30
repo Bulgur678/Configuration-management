414aed3b4bed2df17900db4b28f75d8d66f36f0a
Bulgur678 <abcd505@yandex.ru>
2025-09-30 15:24:30 +0300

    Stage 2: old if-else logic

--- Task1.py
+++ Task1.py
@@ -1,17 +1,24 @@
 import os
+import time
 
 
 def act(a):
     b = a.split()
+    print(b)
     if a == "exit":
         exit()
     if len(b) == 0:
         return ""
-    if  len(b) > 1:
-        if b[0] == "echo":
+    if b[0] == "date":
+        return time.asctime()
+    if b[0].startswith("$"):
+        if (os.environ.get(b[0][1:]) != None):
+            return os.environ.get(b[0][1:])
+    if b[0] == "echo":
+        if  len(b) > 1:
             return a.replace("\n","")[5:]
-        if (os.environ.get(b[1]) != None):
-            b[1] = os.environ.get(b[1])
+        else:
+            return ""
     if b[0] == "ls":
         return b
     elif b[0] == "cd":
@@ -24,7 +31,7 @@ if __name__ == "__main__":
     with open('start_script.txt') as f:
         for line in f:
             res = act(line)
-            print(f"{os.getcwd()} vfs@ {line}{res}")
+            print(f"{os.getcwd()} vfs@: {line}{res}")
             if ("command not found" in res):
                 break
 
